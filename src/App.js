9cc2f036f55d8f6f26431be69592606b5189f64b
bzs-academy <education@bzsacademy.com>
2020-03-04 21:51:55 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './components/Header';
+import GenelView from './components/GenelView';
+
+
+
+function App() {
+  return (
+    <div className="container">
+      <Header />
+      <GenelView />
+    </div>
+  );
+}
+
+export default App;
